a76233c72f2dcb20021fe6ad11c3c3257574163f
kuldeepSunda <kuldeepsundajaat@gmail.com>
2025-07-19 00:02:03 +0530

    Create reduce.js

---
+++ 3 July/reduce.js
@@ -0,0 +1,5 @@
+//Explain what happens in this reduce expression:
+
+[1, 2, 3, 4].reduce((acc, curr) => acc + curr, 0)
+//This expression sums up all the numbers in the array [1, 2, 3, 4].
+//output-10
